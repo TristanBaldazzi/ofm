517155cdaa785d43471383324694660dccb82dba
TristanBaldazzi <tristan.baldazzi@epitech.eu>
2025-01-11 14:57:24 +0100

    Add create modeles button

--- front/app/entreprise/details/[id]/page.tsx
+++ front/app/entreprise/details/[id]/page.tsx
@@ -384,7 +384,15 @@ export default function CompanyDetails() {
             {/* Onglet Modèles */}
             {activeTab === "models" && (
               <>
-                <h2 className="text-xl font-bold mb-4">Liste des Modèles</h2>
+                <div className="flex justify-between items-center mb-6">
+                  <h2 className="text-xl font-bold">Liste des Modèles</h2>
+                  <button
+                    onClick={() => router.push(`/entreprise/model/create/${id}`)}
+                    className="px-4 py-2 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition duration-300"
+                  >
+                    Créer un modèle
+                  </button>
+                </div>
                 {models.length > 0 ? (
                   models.map((model) => (
                     <div key={model._id} className="bg-gray-100 shadow-md rounded-lg p-4 mb-4">
@@ -407,7 +415,6 @@ export default function CompanyDetails() {
                 )}
               </>
             )}
-
 
             {activeTab === "invitation" && (
               <>
